3863fb52f6cc20840906c2bb95cc94b193b9dc6c
RICHMEDIAEMPIRE <richmediaempire@yahoo.com>
2025-07-20 12:50:05 -0400

    catfishempire.com

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: "https://test1243.netlify.app", // Or your real site
+  origin: "https://catfishempire.com", // ✅ Updated to production domain
   credentials: true
 }));
 
